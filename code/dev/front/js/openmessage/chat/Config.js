527920b3603ce95876761e9e87f19c9eb3092110
cyjme <136473394@qq.com>
2018-09-28 22:34:29 +0800

    update

--- code/dev/front/js/openmessage/chat/Config.js
+++ code/dev/front/js/openmessage/chat/Config.js
@@ -1,8 +1,15 @@
 let Config = {
-    'host': '192.168.99.100',
-    'port': '9503',
+    'host': '127.0.0.1',
+    'port': '9001',
     'listContactListUrl': 'http://www.message.jason/open/list-contact',
     'qiniuDomain': 'http://ow7j8d2aj.bkt.clouddn.com/'
 };
+
+// let Config = {
+//     'host': '192.168.99.100',
+//     'port': '9503',
+//     'listContactListUrl': 'http://www.message.jason/open/list-contact',
+//     'qiniuDomain': 'http://ow7j8d2aj.bkt.clouddn.com/'
+// };
 
 export default Config;
